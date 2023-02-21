251e1c5505ca40e772c3dc203335c77f44bf7693
yedige87 <yedabdygapparov@gmail.com>
2023-02-21 15:39:32 +0600

    product.html added, products.py modified

--- shop_app/views/products.py
+++ shop_app/views/products.py
@@ -1,7 +1,8 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import redirect, get_object_or_404, render
+from django.urls import reverse
 
-from shop_app.models import Product
+from shop_app.models import Product, Category
 
 
 def products_view(request: WSGIRequest):
@@ -22,7 +23,22 @@ def category_add_view(request: WSGIRequest):
 
 
 def product_add_view(request: WSGIRequest):
-    pass
+    if request.method == "GET":
+        categories = Category.objects.all()
+        return render(request, 'product_add.html', context={'categories': categories})
+    print(request.POST)
+    prod_data = {
+        'title': request.POST.get('title'),
+        'price': request.POST.get('price'),
+        'photo': request.POST.get('photo'),
+        'cat_id': request.POST.get('cat_id'),
+        'description': request.POST.get('description'),
+    }
+    if prod_data['photo'] == '':
+        prod_data['photo'] = 'blank.jpg'
+
+    product = Product.objects.create(**prod_data)
+    return redirect(reverse('product_view', kwargs={'pk': product.pk}))
 
 
 def category_edit_view(request: WSGIRequest):
